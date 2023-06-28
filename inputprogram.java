187b5d9fe2c67500e420991d41e1b39efa3a4f9b
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-06-28 13:46:15 +0200

    Create inputprogram.java

---
+++ inputprogram.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class inputprogram {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your first name : ");
+        String firstName = input.next();
+
+        System.out.print("Enter your last name : ");
+        String lastName = input.next();
+
+        System.out.println("Your Full Name is " + firstName + " " + lastName);
+    }
+    
+}
