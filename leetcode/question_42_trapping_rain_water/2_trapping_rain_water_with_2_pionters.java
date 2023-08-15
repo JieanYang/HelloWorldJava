4ddd5fec5a9ea8e0c2b0fea913bab94ea2aba4ed
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-08-15 16:27:01 +0200

    Update comment

--- leetcode/question_42_trapping_rain_water/2_trapping_rain_water_with_2_pionters.java
+++ leetcode/question_42_trapping_rain_water/2_trapping_rain_water_with_2_pionters.java
@@ -19,8 +19,7 @@ class trapping_rain_water {
         int right = height.length - 2;
 
         // Key condition: <=, == 的情况是最后一个计算的空间的水的计算，因为所有的 left 和 right 都需要计算，
-        // 如果 left < right 则会漏算一个格子中的水
-        while (left <= right) {
+        while (left <= right) { // 如果 left < right 则会漏算一个格子中的水
             if (maxLeft < maxRight) {
                 if (height[left] >= maxLeft) {
                     maxLeft = height[left];
