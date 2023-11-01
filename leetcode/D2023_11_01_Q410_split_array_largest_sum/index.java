bd9480a3130c3636770043ac83d00b66d6a1df1d
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-11-01 21:51:52 +0100

    add

--- leetcode/D2023_11_01_Q410_split_array_largest_sum/index.java
+++ leetcode/D2023_11_01_Q410_split_array_largest_sum/index.java
@@ -4,10 +4,13 @@ public class index {
     public static void main(String[] args) {
         System.out.println("test");
 
+        int[] nums = { 7, 2, 5, 10, 8 };
+        int m = 2;
+        System.out.println(splitArray(nums, m));
     }
 
-    public int splitArray(int[] nums, int m) {
-        if (nums == null || nums.lenght == 0 || m == 0) {
+    public static int splitArray(int[] nums, int m) {
+        if (nums == null || nums.length == 0 || m == 0) {
             return 0;
         }
 
@@ -31,7 +34,37 @@ public class index {
 
             while (lo < hi) {
                 int mid = lo + (hi - lo) / 2;
+
+                if (isPossible(nums, mid, m)) {
+                    ans = mid;
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+
+            }
+
+            return ans;
+        }
+    }
+
+    public static boolean isPossible(int[] nums, int mid, int m) {
+        int sum = 0;
+        int requiredSubarrays = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+            if (sum > mid) {
+                requiredSubarrays++;
+                sum = nums[i];
             }
         }
+
+        if (requiredSubarrays >= m) {
+            return true;
+        }
+
+        return false;
+
     }
 }
