c3738ac1bd3c6261326801842b826090aff3a689
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-11-01 15:56:40 +0100

    UPdate

--- leetcode/D2023_07_02_Q42_trapping_rain_water/2_trapping_rain_water_with_2_pionters.java
+++ leetcode/D2023_07_02_Q42_trapping_rain_water/2_trapping_rain_water_with_2_pionters.java
@@ -6,7 +6,10 @@ class trapping_rain_water {
 
         int[] input_1 = new int[] { 4, 2, 0, 3, 2, 5 };
 
-        trapping_rain_water.trap(input_1);
+        int sum = trapping_rain_water.trap(input_1);
+
+        System.out.println("sum");
+        System.out.println(sum);
 
     }
 
@@ -19,15 +22,15 @@ class trapping_rain_water {
         int right = height.length - 2;
 
         // Key condition: <=, == 的情况是最后一个计算的空间的水的计算，因为所有的 left 和 right 都需要计算，
-        while (left <= right) { // 如果 left < right 则会漏算一个格子中的水
-            if (maxLeft < maxRight) {
+        while (left <= right) { // 如果 left < right 则会漏算一个格子中的水，因为每次循环都只算单边，需要 == 来满足下一次另一边的计算
+            if (maxLeft < maxRight) { // 右边最高的柱子比左边最高的柱子高
                 if (height[left] >= maxLeft) {
                     maxLeft = height[left];
                 } else {
                     sum += maxLeft - height[left];
                 }
                 left++;
-            } else {
+            } else { // 左边最高的柱子比右边最高的柱子高 或者 一样高
                 if (height[right] >= maxRight) {
                     maxRight = height[right];
                 } else {
--- leetcode/D2023_07_02_Q42_trapping_rain_water/README.md
+++ leetcode/D2023_07_02_Q42_trapping_rain_water/README.md
@@ -11,3 +11,11 @@ https://leetcode.com/problems/trapping-rain-water/description/
 方案中需要做 3 次 loop。
 
 ### Solution 2
+
+方案 2 的所计算的方式其实和方案 1 差不多，唯一不同的是，方案 1 是将所有内容记录下来，无论是否有用。
+
+方案 2 则是动态的维护 maxLeft 和 maxRight 两个变量，有两个大的判断
+
+- 判断是 maxLeft 和 maxRigth 哪边高
+- 然后 math.min(maxLeft, maxRight) 高度更低的地方，即左边低，算 left 和 maxLeft 的关系, 右边低，算 right 和 maxRight 的关系
+- 通过关系并计算当前位置，即 left 或 right 的水量
