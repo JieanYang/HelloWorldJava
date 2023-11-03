fd9f8ce05bc35b56757b7967819fbc19444d3a97
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-11-03 17:45:55 +0100

    Create D2023_11_03_Q852_peak_index_in_a_mountain_array

---
+++ leetcode/D2023_11_03_Q852_peak_index_in_a_mountain_array/README.md
@@ -0,0 +1,7 @@
+## Question
+
+https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+
+## Solution
+
+本题目便利数组，利用 `condition arr[mid-1] < arr[mid] && arr[mid] > arr[mid + 1]` 条件去做判断。
---
+++ leetcode/D2023_11_03_Q852_peak_index_in_a_mountain_array/index.java
@@ -0,0 +1,35 @@
+package leetcode.D2023_11_03_Q852_peak_index_in_a_mountain_array;
+
+class index {
+    public static void main(String[] args) {
+        System.out.println("tes");
+
+        int[] arr = new int[] { 24, 69, 100, 99, 79, 78, 67, 36, 26, 19 };
+        int results = peakIndexInMountainArray(arr);
+
+        System.out.println("results");
+        System.out.println(results);
+    }
+
+    public static int peakIndexInMountainArray(int[] arr) {
+        int left = 1;
+        int right = arr.length - 2;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2; // mid ~= i
+
+            System.out.println("mid");
+            System.out.println(mid);
+
+            // condition arr[mid-1] < arr[mid] && arr[mid] > arr[mid + 1]
+            if (arr[mid - 1] < arr[mid] && arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+}
