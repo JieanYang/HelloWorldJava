14dd5059caaa511359d6fa97acce571f73eead54
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-11-03 17:38:40 +0100

    Create index_2.java, update README.md

--- leetcode/D2023_11_01_Q410_split_array_largest_sum/README.md
+++ leetcode/D2023_11_01_Q410_split_array_largest_sum/README.md
@@ -1,3 +1,9 @@
 ## Question
 
 https://leetcode.com/problems/split-array-largest-sum/description/
+
+## 思路
+
+本题目的思路是从能够分的 subArray 的总和入手，我们去猜可以以 10-32 之间的 mid 去分，在对比分出来的数量 count 是否符合使用者的分法 m。
+
+这是一个将分析某个目标值，转化成判定另一个目标值的巧妙思想。
---
+++ leetcode/D2023_11_01_Q410_split_array_largest_sum/index_2.java
@@ -0,0 +1,63 @@
+package leetcode.D2023_11_01_Q410_split_array_largest_sum;
+
+public class index_2 {
+    public static void main(String[] args) {
+        System.out.println("test");
+
+        int[] nums = { 7, 2, 5, 10, 8 };
+        int m = 2;
+        System.out.println(splitArray(nums, m));
+    }
+
+    public static int splitArray(int[] nums, int k) {
+
+        int low = 0;
+        int hight = 0;
+        for (int i = 0; i < nums.length; i++) {
+            low = Math.max(low, nums[i]);
+            hight += nums[i];
+        }
+
+        System.out.println("To");
+        System.out.println("low");
+        System.out.println(low);
+        System.out.println("hight");
+        System.out.println(hight);
+
+        int left = low;
+        int right = hight;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            System.out.println("mid");
+            System.out.println(mid);
+
+            int count = 1;
+            int sum = 0;
+
+            for (int j = 0; j < nums.length; j++) {
+                int item = nums[j];
+
+                sum = sum + item;
+                if (sum > mid) {
+                    count++;
+                    sum = item;
+                }
+            }
+
+            System.out.println("count");
+            System.out.println(count);
+            System.out.println("left");
+            System.out.println(left);
+
+            if (count > k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+}
