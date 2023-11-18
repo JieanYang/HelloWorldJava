1c2a24577d37733b1c75031c1500c0a0c558074e
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-11-18 16:57:38 +0100

    UPdate

--- leetcode/C_03_linked_list/D2023_11_18_Q141_linked_list_cycle/index.java
+++ leetcode/C_03_linked_list/D2023_11_18_Q141_linked_list_cycle/index.java
@@ -1,7 +1,4 @@
-package leetcode.C_03_linked_list.D2023_11_18_141;
-
-import java.util.ArrayList;
-import java.util.List;
+package leetcode.C_03_linked_list.D2023_11_18_Q141_linked_list_cycle;
 
 import leetcode.C_03_linked_list.D2023_11_18_Q237_delete_node_in_a_linked_list.ListNode;
 
