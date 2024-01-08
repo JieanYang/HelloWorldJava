b1a75bd5866dd9d27ef516f4b71ed3a4637f9842
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-01-08 22:47:29 +0100

    Update

--- leetcode/C_01_array_two_pointers/D2023_11_14_do_5_Q283_move_zeroes/index_2.java
+++ leetcode/C_01_array_two_pointers/D2023_11_14_do_5_Q283_move_zeroes/index_2.java
@@ -10,20 +10,15 @@ public class index_2 {
     }
 
     public void moveZeroes(int[] nums) {
-        int i = 0;
-        int j = 0;
-
-        while (i < nums.length) {
-            if (nums[i] != 0) {
+        int i = 0, j = 0;
+        while (j < nums.length) {
+            if (nums[j] != 0) {
                 int tmp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = tmp;
-
-                j++;
+                i++;
             }
-
-            i++;
+            j++;
         }
-
     }
 }
